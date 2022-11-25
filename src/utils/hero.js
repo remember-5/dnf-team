b0be1f66e91234fd7728a5991b5aaf3fbfcfa4fd
yangjianfei <yangjianfeifly@163.com>
2022-11-25 15:54:02 +0800

    补充职业数据

--- src/utils/hero.js
+++ src/utils/hero.js
@@ -1,6 +1,407 @@
 export const jobs = [
   {
-    id: "1",
-    name: "App 1",
+    key: 1000,
+    label: "鬼剑士·男",
+    children: [
+      {
+        key: 1,
+        label: "剑魂",
+        avatar: require("@/assets/avatar/1.png"),
+      },
+      {
+        key: 2,
+        label: "鬼泣",
+        avatar: require("@/assets/avatar/2.png"),
+      },
+      {
+        key: 3,
+        label: "狂战士",
+        avatar: require("@/assets/avatar/3.png"),
+      },
+      {
+        key: 4,
+        label: "阿修罗",
+        avatar: require("@/assets/avatar/4.png"),
+      },
+      {
+        key: 5,
+        label: "剑影",
+        avatar: require("@/assets/avatar/5.png"),
+      },
+    ],
+  },
+  {
+    key: 1001,
+    label: "鬼剑士·女",
+    children: [
+      {
+        key: 6,
+        label: "驭剑士",
+        avatar: require("@/assets/avatar/6.png"),
+      },
+      {
+        key: 7,
+        label: "暗殿骑士",
+        avatar: require("@/assets/avatar/7.png"),
+      },
+      {
+        key: 8,
+        label: "契魔者",
+        avatar: require("@/assets/avatar/8.png"),
+      },
+      {
+        key: 9,
+        label: "流浪武士",
+        avatar: require("@/assets/avatar/9.png"),
+      },
+      {
+        key: 10,
+        label: "刃影",
+        avatar: require("@/assets/avatar/10.png"),
+      },
+    ],
+  },
+  {
+    key: 1002,
+    label: "格斗家·男",
+    children: [
+      {
+        key: 11,
+        label: "气功师",
+        avatar: require("@/assets/avatar/11.png"),
+      },
+      {
+        key: 12,
+        label: "散打",
+        avatar: require("@/assets/avatar/12.png"),
+      },
+      {
+        key: 13,
+        label: "街霸",
+        avatar: require("@/assets/avatar/13.png"),
+      },
+      {
+        key: 14,
+        label: "柔道家",
+        avatar: require("@/assets/avatar/14.png"),
+      },
+    ],
+  },
+  {
+    key: 1003,
+    label: "格斗家·女",
+    children: [
+      {
+        key: 16,
+        label: "气功师",
+        avatar: require("@/assets/avatar/16.png"),
+      },
+      {
+        key: 17,
+        label: "散打",
+        avatar: require("@/assets/avatar/17.png"),
+      },
+      {
+        key: 18,
+        label: "街霸",
+        avatar: require("@/assets/avatar/18.png"),
+      },
+      {
+        key: 19,
+        label: "柔道家",
+        avatar: require("@/assets/avatar/19.png"),
+      },
+    ],
+  },
+  {
+    key: 1004,
+    label: "神枪手·男",
+    children: [
+      {
+        key: 21,
+        label: "漫游枪手",
+        avatar: require("@/assets/avatar/21.png"),
+      },
+      {
+        key: 22,
+        label: "枪炮师",
+        avatar: require("@/assets/avatar/22.png"),
+      },
+      {
+        key: 23,
+        label: "机械师",
+        avatar: require("@/assets/avatar/23.png"),
+      },
+      {
+        key: 24,
+        label: "弹药专家",
+        avatar: require("@/assets/avatar/24.png"),
+      },
+      {
+        key: 25,
+        label: "合金战士",
+        avatar: require("@/assets/avatar/25.png"),
+      },
+    ],
+  },
+  {
+    key: 1005,
+    label: "神枪手·女",
+    children: [
+      {
+        key: 26,
+        label: "漫游枪手",
+        avatar: require("@/assets/avatar/26.png"),
+      },
+      {
+        key: 27,
+        label: "枪炮师",
+        avatar: require("@/assets/avatar/27.png"),
+      },
+      {
+        key: 28,
+        label: "机械师",
+        avatar: require("@/assets/avatar/28.png"),
+      },
+      {
+        key: 29,
+        label: "弹药专家",
+        avatar: require("@/assets/avatar/29.png"),
+      },
+    ],
+  },
+  {
+    key: 1006,
+    label: "魔法师·男",
+    children: [
+      {
+        key: 31,
+        label: "元素爆破师",
+        avatar: require("@/assets/avatar/31.png"),
+      },
+      {
+        key: 32,
+        label: "冰结师",
+        avatar: require("@/assets/avatar/32.png"),
+      },
+      {
+        key: 33,
+        label: "血法师",
+        avatar: require("@/assets/avatar/33.png"),
+      },
+      {
+        key: 34,
+        label: "逐风者",
+        avatar: require("@/assets/avatar/34.png"),
+      },
+      {
+        key: 35,
+        label: "次元行者",
+        avatar: require("@/assets/avatar/35.png"),
+      },
+    ],
+  },
+  {
+    key: 1007,
+    label: "魔法师·女",
+    children: [
+      {
+        key: 36,
+        label: "元素师",
+        avatar: require("@/assets/avatar/36.png"),
+      },
+      {
+        key: 37,
+        label: "召唤师",
+        avatar: require("@/assets/avatar/37.png"),
+      },
+      {
+        key: 38,
+        label: "战斗法师",
+        avatar: require("@/assets/avatar/38.png"),
+      },
+      {
+        key: 39,
+        label: "魔道学者",
+        avatar: require("@/assets/avatar/39.png"),
+      },
+      {
+        key: 40,
+        label: "小魔女",
+        avatar: require("@/assets/avatar/40.png"),
+      },
+    ],
+  },
+  {
+    key: 1008,
+    label: "圣职者·男",
+    children: [
+      {
+        key: 41,
+        label: "圣骑士",
+        avatar: require("@/assets/avatar/41.png"),
+      },
+      {
+        key: 42,
+        label: "蓝拳圣使",
+        avatar: require("@/assets/avatar/42.png"),
+      },
+      {
+        key: 43,
+        label: "驱魔师",
+        avatar: require("@/assets/avatar/43.png"),
+      },
+      {
+        key: 44,
+        label: "复仇者",
+        avatar: require("@/assets/avatar/44.png"),
+      },
+    ],
+  },
+  {
+    key: 1009,
+    label: "圣职者·女",
+    children: [
+      {
+        key: 46,
+        label: "圣骑士",
+        avatar: require("@/assets/avatar/46.png"),
+      },
+      {
+        key: 47,
+        label: "异端审判者",
+        avatar: require("@/assets/avatar/47.png"),
+      },
+      {
+        key: 48,
+        label: "巫女",
+        avatar: require("@/assets/avatar/48.png"),
+      },
+      {
+        key: 49,
+        label: "诱魔者",
+        avatar: require("@/assets/avatar/49.png"),
+      },
+    ],
+  },
+  {
+    key: 1010,
+    label: "暗夜使者",
+    children: [
+      {
+        key: 51,
+        label: "刺客",
+        avatar: require("@/assets/avatar/51.png"),
+      },
+      {
+        key: 52,
+        label: "死灵术士",
+        avatar: require("@/assets/avatar/52.png"),
+      },
+      {
+        key: 53,
+        label: "忍者",
+        avatar: require("@/assets/avatar/53.png"),
+      },
+      {
+        key: 54,
+        label: "影舞者",
+        avatar: require("@/assets/avatar/54.png"),
+      },
+    ],
+  },
+  {
+    key: 1011,
+    label: "守护者",
+    children: [
+      {
+        key: 56,
+        label: "精灵骑士",
+        avatar: require("@/assets/avatar/56.png"),
+      },
+      {
+        key: 57,
+        label: "混沌魔灵",
+        avatar: require("@/assets/avatar/57.png"),
+      },
+      {
+        key: 58,
+        label: "帕拉丁",
+        avatar: require("@/assets/avatar/58.png"),
+      },
+      {
+        key: 59,
+        label: "龙骑士",
+        avatar: require("@/assets/avatar/59.png"),
+      },
+    ],
+  },
+  {
+    key: 1012,
+    label: "魔枪士",
+    children: [
+      {
+        key: 61,
+        label: "征战者",
+        avatar: require("@/assets/avatar/61.png"),
+      },
+      {
+        key: 62,
+        label: "决战者",
+        avatar: require("@/assets/avatar/62.png"),
+      },
+      {
+        key: 63,
+        label: "狩猎者",
+        avatar: require("@/assets/avatar/63.png"),
+      },
+      {
+        key: 64,
+        label: "暗枪士",
+        avatar: require("@/assets/avatar/64.png"),
+      },
+    ],
+  },
+  {
+    key: 1013,
+    label: "枪剑士",
+    children: [
+      {
+        key: 66,
+        label: "暗刃",
+        avatar: require("@/assets/avatar/66.png"),
+      },
+      {
+        key: 67,
+        label: "特工",
+        avatar: require("@/assets/avatar/67.png"),
+      },
+      {
+        key: 68,
+        label: "战线佣兵",
+        avatar: require("@/assets/avatar/68.png"),
+      },
+      {
+        key: 69,
+        label: "源能专家",
+        avatar: require("@/assets/avatar/69.png"),
+      },
+    ],
+  },
+  {
+    key: 1014,
+    label: "外传职业",
+    children: [
+      {
+        key: 71,
+        label: "缔造者",
+        avatar: require("@/assets/avatar/71.png"),
+      },
+      {
+        key: 72,
+        label: "黑暗武士",
+        avatar: require("@/assets/avatar/72.png"),
+      },
+    ],
   },
 ];
